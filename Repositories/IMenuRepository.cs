da1dc42baac4285a3c3a6ebcb661855d430fd4dd
Testpilot228 <denny.sikora@gmx.de>
2026-08-12 10:02:01 +0200

    add comment

--- Repositories/IMenuRepository.cs
+++ Repositories/IMenuRepository.cs
@@ -4,12 +4,18 @@ namespace MenuAPI.Repositories
 {
     public interface IMenuRepository
     {
+
         Task<List<Menu>> GetAllMenusAsync();
 
         Task<Menu?> GetMenuByIdAsync(int id);
 
         Task<Menu?> GetMenuByDateAsync(DateOnly date);
 
+        /***
+         * Create a new menu in the database.
+         * @param menu The menu to create.
+         * @return The created menu with its ID and internal creation of data like days and meals.
+         */
         Task<Menu> CreateMenuAsync(Menu menu);
 
         Task<bool> DeleteMenuAsync(int id);
