76c1c1ec91a7c783943f191933e488bc880ef394
Testpilot228 <denny.sikora@gmx.de>
2026-09-01 09:46:25 +0200

    add DB-Migration for ReciptURL and ImageURL

--- Migrations/20260901074547_ReceiptURLandImageURL.Designer.cs
+++ Migrations/20260901074547_ReceiptURLandImageURL.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MenuAPI.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MenuAPI.Migrations
 {
     [DbContext(typeof(MenuDbContext))]
-    partial class MenuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260901074547_ReceiptURLandImageURL")]
+    partial class ReceiptURLandImageURL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260901074547_ReceiptURLandImageURL.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenuAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReceiptURLandImageURL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Meal",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiptURL",
+                table: "Meal",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Meal");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiptURL",
+                table: "Meal");
+        }
+    }
+}
